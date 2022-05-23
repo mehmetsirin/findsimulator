e7fba664992a2422d9d9b26460084af640fc15f0
mehmetsirin <mhmtsirinsk@gmail.com>
2022-05-24 00:18:23 +0300

    Revert "Revert "Revert "Revert "Revert "WDWE"""""
    This reverts commit d20230301fe369e0fac90a4fc3bb338559f360a3.

--- FindSimulator/FindSimulator.Api/Controllers/TestController.cs
+++ FindSimulator/FindSimulator.Api/Controllers/TestController.cs
@@ -23,7 +23,6 @@ namespace FindSimulator.Api.Controllers
 
         private readonly IEventBus eventBus;
         public int MyProperty { get; set; }
-
         public TestController(IEventBus eventBus)
         {
             this.eventBus = eventBus;
